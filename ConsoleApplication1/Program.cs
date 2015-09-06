88a04071c184fb71753cf70aa69bde6cdc51e86e
Arnav Gupta <dev@championswimmer.in>
2015-09-06 13:57:57 +0530

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,72 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+        static string ApplicationName = "Gmail API Quickstart";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials-gmail");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Gmail API service.
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            UsersResource.ThreadsResource.ListRequest request = service.Users.Threads.List("me");
+
+            // List threads.
+            IList<Google.Apis.Gmail.v1.Data.Thread> threads = request.Execute().Threads;
+            Console.WriteLine("Emails:");
+            if (threads != null && threads.Count > 0)
+            {
+                foreach (var threadItem in threads)
+                {
+                    Console.WriteLine("{0}", threadItem.Snippet);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No emails found.");
+            }
+            Console.Read();
+
+        }
+    }
+
+}
